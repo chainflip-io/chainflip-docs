c1401b360803e24d6cb0006e7e880fc484454a85
Zoheb Salam <zoheb@chainflip.io>
2023-09-05 11:52:17 +0200

    add perseverance docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,15 +1,28 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
+const logoStyle = {
+  display: "flex",
+  alignItems: "center",
+  fontSize: "1.45rem",
+  fontWeight: "bold",
+};
+
 const config: DocsThemeConfig = {
-  logo: <span>Chainflip Docs</span>,
+  logo: (
+    <div style={logoStyle}>
+      <img height="25" width="25" src="/chainfliplogo.png" />
+      <span style={{ marginLeft: "8px" }}>Chainflip Docs</span>
+    </div>
+  ),
   project: {
     link: "https://github.com/chainflip-io",
   },
   chat: {
     link: "https://discord.com/channels/824147014140952596/824147014140952602",
   },
-  docsRepositoryBase: "https://github.com/chainflip-io/chainflip-docs",
+  docsRepositoryBase:
+    "https://github.com/chainflip-io/chainflip-docs/blob/main",
   footer: {
     text: "Chainflip Docs",
   },
